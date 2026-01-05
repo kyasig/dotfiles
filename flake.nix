856a5fed31d6035376e12f6822081556e849a594
kyasig <kevinyang177013@gmail.com>
2026-01-04 19:06:40 -0800

    fix typo in flake.nix

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixmobar.url = "git+https://codeberg.org/xmobar/xmobar.git/?dir=nix";
-    };
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
